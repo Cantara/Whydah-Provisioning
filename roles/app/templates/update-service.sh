04a90a451198197613788eeeb95efbc74d200187
Sherriff <erik-dev@fjas.no>
2015-06-29 10:02:15 +0200

    #1 - delete old jar files (keep 4) when downloading new artifacts + a few minor adjustments to improve readability.

--- roles/app/templates/update-service.sh
+++ roles/app/templates/update-service.sh
@@ -1,30 +1,42 @@
 #!/bin/sh
+# Script to download deployment unit from a Maven artifact repository.
 
-A={{app_name}}
+releaseRepo={{maven_releases_repository}}
+snapshotRepo={{maven_snapshot_repository}}
+groupId={{group_id}}
+artifactId={{app_name}}
 V={{version}}
 
 
 if [[ $V == *SNAPSHOT* ]]; then
-   echo Note: If the artifact version contains "SNAPSHOT" - the artifact latest greates snapshot is downloaded, Irrelevent of version number!!!
-   path="{{maven_snapshot_repository}}/{{group_id}}/$A"
+   echo Note: If the artifact version contains "SNAPSHOT", the latest snapshot version is downloaded, ignoring the version before SNAPSHOT.
+   path="$snapshotRepo/$groupId/$artifactId"
    version=`curl -s "$path/maven-metadata.xml" | grep "<version>" | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/" | tail -n 1`
    echo "Version $version"
    build=`curl -s "$path/$version/maven-metadata.xml" | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-   JARFILE="$A-$build.jar"
+   JARFILE="$artifactId-$build.jar"
    url="$path/$version/$JARFILE"
 else #A specific Release version
-   path="{{maven_releases_repository}}/{{group_id}}/$A"
-   url=$path/$V/$A-$V.jar
-   JARFILE=$A-$V.jar
+   path="releaseRepo/$groupId/$A"
+   url=$path/$V/$artifactId-$V.jar
+   JARFILE=$artifactId-$V.jar
 fi
 
-# Download
+# Download artifact
 echo Downloading $url
 wget -O $JARFILE -q -N $url
 
 
-#Create symlink or replace existing sym link
+# Create symlink or replace existing sym link
 if [ -h $A.jar ]; then
    unlink $A.jar
 fi
 ln -s $JARFILE $A.jar
+
+# Delete old jar files
+jar=$artifactId*.jar
+nrOfJarFilesToDelete=`ls $jar -A1t | tail -n +6 | wc -l`
+if [[ $nrOfJarFilesToDelete > 0 ]]; then
+    echo Deleting $nrOfJarFilesToDelete old jar files. Keep the 4 newest + the symlink.
+    ls $jar -A1t | tail -n +6 | xargs rm
+fi
