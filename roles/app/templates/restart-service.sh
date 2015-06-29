8d954ba0eff510e8b78b71dda73f9e3d061fe172
Sherriff <erik-dev@fjas.no>
2015-06-29 10:31:03 +0200

    #1 - go back to initctl stop for restart-service.sh

--- roles/app/templates/restart-service.sh
+++ roles/app/templates/restart-service.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Manual script to restart application.
-sudo pkill -u {{username}} -f {{app_name}}
+#sudo pkill -u {{username}} -f {{app_name}}
+sudo initctl stop {{app_name}}
 sudo initctl start {{app_name}}
